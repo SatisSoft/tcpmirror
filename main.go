b06740c3a094e71bcd9b29eb882277a00d3dd997
ashirko <antonshirko@frtk.ru>
2018-07-10 13:41:29 +0300

    fixed

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func main() {
+	fmt.Println("start execution")
 	for num := 0; num < 1; num++ {
 		go func(num int) {
+			fmt.Println("start execution")
 			c, err := redis.Dial("tcp", ":6379")
-			if err != nill {
+			if err != nil {
 				fmt.Printf("error in %d connection: %s\n", num, err)
 				return
 			}
@@ -21,32 +23,32 @@ func main() {
 			for i := 0; i < 10; i++ {
 				val := strconv.Itoa(num) + ":" + strconv.FormatInt(millisecs, 10)
 				_, err = c.Do("ZADD", num, millisecs, val)
-				if err != null {
+				if err != nil  {
 					fmt.Printf("error in request for %d connection: %s", num, err)
 					return
 				}
 
 			}
 			res1, err := c.Do("ZRANGE", num, 0, 0)
-			if err != null {
+			if err != nil  {
 				fmt.Printf("error in request for %d connection: %s", num, err)
 				return
 			}
 			fmt.Printf("%d connection result 1: %v", num, res1)
 			res2, err := c.Do("ZRANGE", num, 0, 2)
-			if err != null {
+			if err != nil  {
 				fmt.Printf("error in request for %d connection: %s", num, err)
 				return
 			}
 			fmt.Printf("%d connection result 2: %v", num, res2)
 			res3, err := c.Do("ZRANGEBYSCORE", num, 0, getMill())
-			if err != null {
+			if err != nil  {
 				fmt.Printf("error in request for %d connection: %s", num, err)
 				return
 			}
 			fmt.Printf("%d connection result 3: %v", num, res3)
 			res4, err := c.Do("ZRANGEBYSCORE", num, 0, getMill())
-			if err != null {
+			if err != nil  {
 				fmt.Printf("error in request for %d connection: %s", num, err)
 				return
 			}
