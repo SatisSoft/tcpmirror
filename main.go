bcf1b22e8934674bc57df8ddf50ae276c5baf9c4
ashirko <antonshirko@frtk.ru>
2018-07-10 15:41:08 +0300

    test

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"strconv"
-	"time"
 	"sync"
+	"time"
 )
+
+const max = 10
 
 func main() {
 	fmt.Println("start execution")
 	var wg sync.WaitGroup
-	for num := 0; num < 1; num++ {
+	for num := 0; num < max; num++ {
 		wg.Add(1)
 		go func(num int) {
 			defer wg.Done()
@@ -23,40 +25,16 @@ func main() {
 			}
 			defer c.Close()
 			fmt.Printf("%d connection established\n", num)
-			millisecs := getMill()
-			for i := 0; i < 10; i++ {
-				val := strconv.Itoa(num) + ":" + strconv.FormatInt(millisecs, 10) + ":" + strconv.Itoa(i)
+			for {
+				millisecs := getMill()
+				val := strconv.FormatInt(time.Now().UnixNano(), 10)
 				_, err = c.Do("ZADD", num, millisecs, val)
-				if err != nil  {
+				if err != nil {
 					fmt.Printf("error in request for %d connection: %s\n", num, err)
 					return
 				}
-
+				time.Sleep(1 * time.Second)
 			}
-			res1, err := c.Do("ZRANGE", num, 0, 0)
-			if err != nil  {
-				fmt.Printf("error in request for %d connection: %s\n", num, err)
-				return
-			}
-			fmt.Printf("%d connection result 1: %v\n", num, res1)
-			res2, err := c.Do("ZRANGE", num, 0, 2)
-			if err != nil  {
-				fmt.Printf("error in request for %d connection: %s\n", num, err)
-				return
-			}
-			fmt.Printf("%d connection result 2: %v\n", num, res2)
-			res3, err := c.Do("ZRANGEBYSCORE", num, 0, getMill())
-			if err != nil  {
-				fmt.Printf("error in request for %d connection: %s\n", num, err)
-				return
-			}
-			fmt.Printf("%d connection result 3: %v\n", num, res3)
-			res4, err := c.Do("ZRANGEBYSCORE", num, 0, getMill()-1000)
-			if err != nil  {
-				fmt.Printf("error in request for %d connection: %s\n", num, err)
-				return
-			}
-			fmt.Printf("%d connection result 4: %v\n", num, res4)
 		}(num)
 	}
 	wg.Wait()
