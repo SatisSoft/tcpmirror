5d476067b5726419ce84c454a071cc7b30e5a38e
egorban <gorban@satissoft.ru>
2018-08-07 16:22:37 +0300

    testing fun

--- egts_test.go
+++ egts_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-func TestParser(t *testing.T) {
+func TestFormEGTS(t *testing.T) {
 	packetOrig:=[]byte{1,0,0,11,0,35,0,0,0,1,153,
 						24,0,0,0,1,239,0,0,0,2,2,
 						16,21, 0,210,49,43,16,79,186,58,158,210,39,188,53,3,0,0,178,0,0,0,0,0,106,141}
