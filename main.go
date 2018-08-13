131a6e66c841a3c501a8fd91c47aee6cc780d559
egorban <gorban@satissoft.ru>
2018-08-13 18:50:21 +0300

    processing several egts packets

--- main.go
+++ main.go
@@ -221,16 +221,16 @@ func waitReplyEGTS() {
 				log.Printf("error while getting reply from client %s", err)
 				go egtsConStatus()
 			}
-			egtsReqID, err := parseEGTS(b[:n])
+			egtsReqIDs, err := parseEGTS(b[:n])
 			if err != nil {
 				log.Printf("error while parsing reply from EGTS %v: %s", b[:n], err)
-			} else {
-				err := deleteEGTS(cR, egtsReqID)
+			}
+			for id := range egtsReqIDs {
+				err := deleteEGTS(cR, id)
 				if err != nil {
 					log.Printf("error while delete EGTS id %s", err)
 				}
 			}
-
 		}
 	}
 }
