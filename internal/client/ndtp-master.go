09addbaec4b06e10f0d9b5b5495469a2c9e79eb0
gorban <>
2021-04-15 19:45:31 +0300

    fix

--- internal/client/ndtp-master.go
+++ internal/client/ndtp-master.go
@@ -144,10 +144,13 @@ func (c *NdtpMaster) clientLoop() {
 		select {
 		case message := <-c.Input:
 			if c.open {
+
 				ticker.Stop()
 				monitoring.SendMetric(c.Options, c.monTable, monTags, monitoring.QueuedPkts, len(c.Input))
 				c.handleMessageRealtime(message)
 				ticker = time.NewTicker(time.Duration(PeriodSendOnlyOldNdtpMs) * time.Millisecond)
+			} else {
+				c.logger.Println("skip msg")
 			}
 		case <-ticker.C:
 			if c.open {
